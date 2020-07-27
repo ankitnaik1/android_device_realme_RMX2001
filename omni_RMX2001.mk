87e6cbce8889f67a65586a4d6f20ff99089a838a
hpnightowl <iamhp2k@gmail.com>
2020-07-27 21:57:13 +0530

    RMX2001: TEMP Changes

--- omni_RMX2001.mk
+++ omni_RMX2001.mk
@@ -17,21 +17,14 @@
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/embedded.mk)
 
-# Inherit Telephony packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit language packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Inherit 64bit support
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
 # A/B
 AB_OTA_PARTITIONS += \
@@ -72,3 +65,11 @@ PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme 6
 PRODUCT_MANUFACTURER := realme
 PRODUCT_RELEASE_NAME := Realme 6
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.bootimage.build.date.utc \
+    ro.build.date.utc
